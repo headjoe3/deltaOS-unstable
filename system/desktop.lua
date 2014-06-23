bf1f0888a6ee98a1a92536d3f04fcee04aa54d8e
FlareHAX0R <gameexpertnetwork@gmail.com>
2014-06-23 19:51:23 -0400

    Create desktop.lua

---
+++ system/desktop.lua
@@ -0,0 +1,26 @@
+os.pullEvent = os.pullEventRaw
+
+
+term.setBackgroundColor(colors.lightBlue)
+term.clear()
+term.setCursorPos(1, kernel.y)
+
+kernel.drawLine(kernel.y, colors.lightGray)
+
+term.setCursorPos(1, kernel.y)
+
+write("D")
+
+os.loadAPI("/system/tabserv")
+
+while true do
+	if kernel.clickEvent(1, kernel.y, 2) then
+		local d = dialog.new(nil, nil, nil, nil, "DeltaOS", {"Do you want to", "launch the shell?"}, true, false)
+		if d:autoCaptureEvents() == true or d:autoCaptureEvents() == "ok" then
+			tabserv.tabRun("/rom/programs/shell", "Shell")
+		end
+	end
+end
+
+
+

29a40d837107f5fcd97c874674641899c5b94216
FlareHAX0R <gameexpertnetwork@gmail.com>
2014-06-25 21:49:38 -0400

    Update desktop.lua

--- system/desktop.lua
+++ system/desktop.lua
@@ -16,6 +16,15 @@ end
 
 draw()
 
+
+local function sleep()
+ for i=1, kernel.y do
+  paitnutils.drawLine(i, colors.black)
+  sleep(0.5)
+ end
+end
+
+
 local function PtF(file)
  local data = fs.open(file, "r")
  dataA = data:readAll()
@@ -39,4 +48,32 @@ while true do
 end
 end
 
-shellServ()
+
+local function sleepServ()
+ while true do
+ 	local event, key = os.pullEvent("key")
+ 	if key == 59 then
+ 		sleep()
+ 		local event = os.pullEvent()
+ 		if event == "key" or event == "mouse_click" then
+ 			draw()
+ 		end
+ 	end
+ end
+end
+
+parallel.waitForAll(sleepServ, shellServ)
+
+
+
+
+
+
+
+
+
+
+
+
+
+

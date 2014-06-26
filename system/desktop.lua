d03f721c3cd0d85d8c5ea7d3ea9d04609f22ee54
FlareHAX0R <gameexpertnetwork@gmail.com>
2014-06-26 12:52:25 -0400

    Update desktop.lua

--- system/desktop.lua
+++ system/desktop.lua
@@ -1,17 +1,63 @@
 os.pullEvent = os.pullEventRaw
 
+
+
+
 dofile("/system/sApi/dialog")
 
 local function draw()
 graphics.reset(colors.lightBlue)
 term.setCursorPos(1, kernel.y)
 
-graphics.drawLine(kernel.y, colors.lightGray)
+graphics.drawLine(1, colors.lightGray)
 
-term.setCursorPos(1, kernel.y)
+term.setCursorPos(1, 1)
 
 write("D")
 end
+
+
+local function shellDialog()
+local d = Dialog.new(nil, nil, nil, nil, "DeltaOS", {"Do you want to", "launch the shell?"}, true,true)
+		if d:autoCaptureEvents() == "ok" then
+			draw()
+			animations.closeIn()
+			graphics.reset(colors.black, colors.white)
+			
+			print("Run 'exit' to go back to deltaOS")
+			shell.run("/rom/programs/shell")
+			animations.wake()
+			draw()
+		else
+			
+			draw()
+		end
+end
+
+
+local function sleepX()
+ animations.sleep()
+ while true do
+ 		local event = os.pullEvent()
+ 		if event == "key" or event == "mouse_click" then
+ 			os.sleep(0.1)
+ 			animations.wake()
+ 			os.sleep(0.1)
+ 			draw()
+ 		end
+ end
+end
+
+local menuData = {
+
+[1] = { text = "Shut Down", handler = function() animations.sleep() os.shutdown() end },
+[2] = { text = "Reboot", handler = function() animations.sleep() os.reboot() end },
+[3] = { text = "Sleep", handler = function() sleepX() end },
+[4] = { text = "Shell", handler = function() shellDialog() end }
+
+}
+
+
 --os.loadAPI("/system/tabserv")
 
 draw()
@@ -26,9 +72,10 @@ local function PtF(file)
  return dataA
 end
 
+--[[
 local function shellServ() 
 while true do
-	if kernel.clickEvent(1, kernel.y, 2) then
+	if kernel.clickEvent(1, 1, 2) then
 		local d = Dialog.new(nil, nil, nil, nil, "DeltaOS", {"Do you want to", "launch the shell?"}, true,true)
 		if d:autoCaptureEvents() == "ok" then
 			draw()
@@ -46,6 +93,18 @@ while true do
 	end
 end
 end
+]]--
+
+local function menuServ()
+	while true do
+		if kernel.clickEvent(1, 1, 1 ) then
+			term.setCursorPos(1, 1)
+			menu.runMenu(menuData)
+			menu.stopMenu()
+		end
+	end
+end
+			
 
 
 local function sleepServ()
@@ -64,7 +123,7 @@ local function sleepServ()
  end
 end
 
-parallel.waitForAll(sleepServ, shellServ)
+parallel.waitForAll(sleepServ, menuServ)
 
 
 

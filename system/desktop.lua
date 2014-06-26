bce54dd9b6754af6b47a26beac8da0b12a91d325
FlareHAX0R <gameexpertnetwork@gmail.com>
2014-06-26 13:00:41 -0400

    Update desktop.lua

--- system/desktop.lua
+++ system/desktop.lua
@@ -17,45 +17,10 @@ write("D")
 end
 
 
-local function shellDialog()
-local d = Dialog.new(nil, nil, nil, nil, "DeltaOS", {"Do you want to", "launch the shell?"}, true,true)
-		if d:autoCaptureEvents() == "ok" then
-			draw()
-			animations.closeIn()
-			graphics.reset(colors.black, colors.white)
-			
-			print("Run 'exit' to go back to deltaOS")
-			shell.run("/rom/programs/shell")
-			animations.wake()
-			draw()
-		else
-			
-			draw()
-		end
-end
 
 
-local function sleepX()
- animations.sleep()
- while true do
- 		local event = os.pullEvent()
- 		if event == "key" or event == "mouse_click" then
- 			os.sleep(0.1)
- 			animations.wake()
- 			os.sleep(0.1)
- 			draw()
- 		end
- end
-end
 
-local menuData = {
 
-[1] = { text = "Shut Down", handler = function() animations.sleep() os.shutdown() end },
-[2] = { text = "Reboot", handler = function() animations.sleep() os.reboot() end },
-[3] = { text = "Sleep", handler = function() sleepX() end },
-[4] = { text = "Shell", handler = function() shellDialog() end }
-
-}
 
 
 --os.loadAPI("/system/tabserv")

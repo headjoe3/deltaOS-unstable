98ecaa186f286c5350c92eded1bade77ecd161f0
FlareHAX0R <gameexpertnetwork@gmail.com>
2014-06-25 22:38:47 -0400

    Update desktop.lua

--- system/desktop.lua
+++ system/desktop.lua
@@ -24,13 +24,19 @@ local function sleep()
  end
 end
 
+local function closeIn()
+ for i=1, kernel.y do
+  paintutils.drawLine(i, 1, i, kernel.y, colors.black) 
+  os.sleep(0.01)
+ end
+end
+
 local function wake()
  for i=1, kernel.y do
   graphics.drawLine(i, colors.lightBlue)
   os.sleep(0.01)
  end
 end
-
 
 local function PtF(file)
  local data = fs.open(file, "r")
@@ -44,7 +50,9 @@ while true do
 	if kernel.clickEvent(1, kernel.y, 2) then
 		local d = Dialog.new(nil, nil, nil, nil, "DeltaOS", {"Do you want to", "launch the shell?"}, true,true)
 		if d:autoCaptureEvents() == "ok" then
+			closeIn()
 			graphics.reset(colors.black, colors.white)
+			
 			print("Run 'exit' to go back to deltaOS")
 			shell.run("/rom/programs/shell")
 			draw()

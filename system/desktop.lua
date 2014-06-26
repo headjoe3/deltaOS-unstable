bffc3486a7742f7c1e6371c75f15ce801e356f6a
FlareHAX0R <gameexpertnetwork@gmail.com>
2014-06-25 20:25:36 -0400

    Update desktop.lua

--- system/desktop.lua
+++ system/desktop.lua
@@ -3,11 +3,10 @@ os.pullEvent = os.pullEventRaw
 dofile("/system/sApi/dialog")
 
 local function draw()
-term.setBackgroundColor(colors.lightBlue)
-term.clear()
+graphics.reset(colors.lightBlue)
 term.setCursorPos(1, kernel.y)
 
-kernel.drawLine(kernel.y, colors.lightGray)
+graphics.drawLine(kernel.y, colors.lightGray)
 
 term.setCursorPos(1, kernel.y)
 
@@ -29,11 +28,7 @@ while true do
 	if kernel.clickEvent(1, kernel.y, 2) then
 		local d = Dialog.new(nil, nil, nil, nil, "DeltaOS", {"Do you want to", "launch the shell?"}, true,true)
 		if d:autoCaptureEvents() == "ok" then
-			term.setBackgroundColor(colors.black)
-			term.setTextColor(colors.white)
-			term.clear()
-			term.setCursorPos(1, 1)
-			print("Type 'exit' to go back to deltaOS")
+			print("Run 'edit' to go back to deltaOS")
 			shell.run("/rom/programs/shell")
 			draw()
 		else

91acfe9a963bc33e268567b93dd935bd37b5baa7
FlareHAX0R <gameexpertnetwork@gmail.com>
2014-06-26 09:12:32 -0400

    Update desktop.lua

--- system/desktop.lua
+++ system/desktop.lua
@@ -17,26 +17,7 @@ end
 draw()
 
 
-local function sleep()
- for i=1, kernel.y do
-  graphics.drawLine(i, colors.black)
-  os.sleep(0.01)
- end
-end
 
-local function closeIn()
- for i=1, kernel.y do
-  paintutils.drawLine(i, 1, i, kernel.y, colors.black) 
-  os.sleep(0.01)
- end
-end
-
-local function wake()
- for i=1, kernel.y do
-  graphics.drawLine(i, colors.lightBlue)
-  os.sleep(0.01)
- end
-end
 
 local function PtF(file)
  local data = fs.open(file, "r")
@@ -50,7 +31,7 @@ while true do
 	if kernel.clickEvent(1, kernel.y, 2) then
 		local d = Dialog.new(nil, nil, nil, nil, "DeltaOS", {"Do you want to", "launch the shell?"}, true,true)
 		if d:autoCaptureEvents() == "ok" then
-			closeIn()
+			animations.closeIn()
 			graphics.reset(colors.black, colors.white)
 			
 			print("Run 'exit' to go back to deltaOS")
@@ -69,11 +50,11 @@ local function sleepServ()
  while true do
  	local event, key = os.pullEvent("key")
  	if key == 59 then
- 		sleep()
+ 		animations.sleep()
  		local event = os.pullEvent()
  		if event == "key" or event == "mouse_click" then
  			os.sleep(0.1)
- 			wake()
+ 			animations.wake()
  			os.sleep(0.1)
  			draw()
  		end

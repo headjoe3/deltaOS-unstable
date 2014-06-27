6e2d8f836ce32c254eb68fa3f1883c76d240801c
FlareHAX0R <gameexpertnetwork@gmail.com>
2014-06-27 11:52:56 -0400

    Update ft-setup.lua

--- ft-setup.lua
+++ ft-setup.lua
@@ -8,7 +8,7 @@ print("")
 
 paintutils.drawLine(2, kernel.y/2, kernel.x-1, kernel.y/2, colors.gray)
 
-paintutils.drawLine(2, kernel.y/2-4, kernel.x-1, kernel.y/2-4, colors.gray)
+paintutils.drawLine(2, kernel.y/2+4, kernel.x-1, kernel.y/2+4, colors.gray)
 
 term.setCursorPos(1, kernel.y/2-1)
 term.setBackgroundColor(colors.lightGray)
@@ -16,7 +16,7 @@ term.setBackgroundColor(colors.lightGray)
 
 graphics.cPrint("Username:")
 
-term.setCursorPos(1, kernel.y/2-3)
+term.setCursorPos(1, kernel.y/2+3)
 
 graphics.cPrint("Password: ")
 
@@ -24,7 +24,7 @@ term.setCursorPos(2, kernel.y/2)
 term.setBackgroundColor(colors.gray)
 local user = read()
 
-term.setCursorPos(2, kernel.y/2-4)
+term.setCursorPos(2, kernel.y/2+4)
 
 local pass = read("*")
 

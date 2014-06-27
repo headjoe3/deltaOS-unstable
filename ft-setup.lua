40257c6bf886f2be8fda14b48af6ec62b5c40a04
FlareHAX0R <gameexpertnetwork@gmail.com>
2014-06-27 11:38:46 -0400

    Create ft-setup.lua

---
+++ ft-setup.lua
@@ -0,0 +1,63 @@
+graphics.reset(colors.lightGray, colors.black)
+graphics.cPrint("DeltaOS first-time setup")
+
+
+print("")
+graphics.cPrint("I. User setup")
+print("")
+
+paintutils.drawLine(2, kernel.y/2, kernel.x-1, kernel.y/2, colors.gray)
+
+paintutils.drawLine(2, kernel.y/2-4, kernel.x-1, kernel.y/2-4, colors.gray)
+
+term.setCursorPos(1, kernel.y/2-1)
+term.setBackgroundColor(colors.lightGray)
+
+
+graphics.cPrint("Username:")
+
+term.setCursorPos(1, kernel.y/2-3)
+
+graphics.cPrint("Password: ")
+
+term.setCursorPos(2, kernel.y/2)
+term.setBackgroundColor(colors.gray)
+local user = read()
+
+term.setCursorPos(2, kernel.y/2-4)
+
+local pass = read("*")
+
+user.newUser(user, pass)
+
+graphics.reset(colors.lightGray, colors.black)
+
+graphics.cPrint("DeltaOS first-time setup")
+print("")
+graphics.cPrint("II. Computer name")
+
+paintutils.drawLine(2, kernel.y/2, kernel.x-1, kernel.y/2, colors.gray)
+
+
+term.setCursorPos(1, kernel.y/2-2)
+
+term.setBackgroundColor(colors.lightGray)
+
+graphics.cPrint("Computer label/name: ")
+
+term.setCursorPos(2, kernel.y/2)
+
+term.setBackgroundColor(colors.gray)
+
+local label = read()
+
+os.setComputerLabel(label)
+
+graphics.reset(colors.lightGray, colors.black)
+
+graphics.cPrint("First time setup is finished.")
+graphics.cPrint("DeltaOS will reboot.")
+
+
+
+

24f3f5bcc5a3fc6def2c634117c78eeec45137f6
FlareHAX0R <gameexpertnetwork@gmail.com>
2014-06-29 14:06:52 -0400

    Update desktop.lua

--- system/desktop.lua
+++ system/desktop.lua
@@ -64,7 +64,7 @@ login_main()
 dofile("/system/sApi/dialog")
 
 local function draw()
-graphics.reset( tonumber(users.getUserSetting( users.getUserName(), "desktopColor") ) )
+graphics.reset( colors.ligthBlue )
 term.current().setCursorPos(1, kernel.y)
 
 graphics.drawLine(1, colors.lightGray)
@@ -74,7 +74,7 @@ term.current().setCursorPos(1, 1)
 write("D")
 
 if isUnstable then
- term.current().setBackgroundColor( tonumber(users.getUserSetting( users.getUserName(), "desktopColor") ) )
+ term.current().setBackgroundColor( colors.lightBlue )
  term.current().setCursorPos(kernel.x-string.len(fullBuildName), kernel.y)
  write(fullBuildName)
  term.current().setCursorPos(1, 1)
